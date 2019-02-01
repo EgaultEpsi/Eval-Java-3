1c684ae91b7929dae93f27ff62f330b00e5e0bcb
egaul <egaul@DESKTOP-4HUM0JC>
2019-02-01 21:57:24 +0100

    Projet avec Partie 1&2

--- src/fr/epsi/book/dal/BookDAO.java
+++ src/fr/epsi/book/dal/BookDAO.java
@@ -6,32 +6,32 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class BookDAO implements IDAO<Book, Long> {
-	
+
 	@Override
-	public void create( Book o ) throws SQLException {
-		//TODO
+	public void create(Book o) throws SQLException {
+		// TODO
 	}
-	
+
 	@Override
-	public Book findById( Long aLong ) {
-		//TODO
+	public Book findById(Long aLong) {
+		// TODO
 		return null;
 	}
-	
+
 	@Override
 	public List<Book> findAll() {
-		//TODO
+		// TODO
 		return null;
 	}
-	
+
 	@Override
-	public Book update( Book o ) {
-		//TODO
+	public Book update(Book o) {
+		// TODO
 		return null;
 	}
-	
+
 	@Override
-	public void remove( Book o ) {
-		//TODO
+	public void remove(Book o) {
+		// TODO
 	}
 }
